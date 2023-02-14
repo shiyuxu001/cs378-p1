ce9e58b30782de4f7c87fb88fd88396a571f6393
shiyuxu001 <shiyuxu001@gmail.com>
2023-02-14 04:03:24 -0600

    added script

---
+++ script.js
@@ -0,0 +1,12 @@
+// //fetch the button from the DOM
+// var button = document.getElementById("button");
+
+// //attach and event when the user clicks it
+// button.addEventListener("click", myfunction);
+
+// //create the function that will be called when the button is pressed
+// function myfunction()
+// {
+// 	//this shows a popup window
+// 	alert("button clicked!");
+// }
