3255ef4e255c7cdad0e0ab93a779ec6dfcc68763
shiyuxu001 <shiyuxu001@gmail.com>
2023-02-14 15:00:13 -0600

    bruh"

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+ alert("hi");
 // //fetch the button from the DOM
-// var button = document.getElementById("button");
 
+// var hotdog_btn = document.getElementById("btnone");
+// var subtotal = 0;
 // //attach and event when the user clicks it
-// button.addEventListener("click", myfunction);
+// hotdog_btn.addEventListener("click", myfunction);
 
 // //create the function that will be called when the button is pressed
 // function myfunction()
 // {
+//     console.log("hotdog pos btn");
 // 	//this shows a popup window
+//     var txt = document.getElementById("hotdog");
+//     txt.textContent = 11;
 // 	alert("button clicked!");
 // }
